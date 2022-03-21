eec3c8c9e27732c699eefadfd273e56937fe6a87
lucas-maia-morais <lucasmaiamorais@gmail.com>
2022-03-21 15:09:04 -0300

    New Images and logic for cross the side line

--- car_game.py
+++ car_game.py
@@ -1,4 +1,4 @@
-from turtle import width
+from turtle import back, width
 import pygame
 pygame.init()
 
@@ -10,12 +10,30 @@ screen = pygame.display.set_mode((width,height))
 # loading the image
 carimg = pygame.image.load("car1.jpg")
 
+# width of the car
+car_width = 56
+
+# loading all images
+grass = pygame.image.load("grass.jpg")
+yellow_strip = pygame.image.load("yellow_strip.jpg")
+strip = pygame.image.load("strip.jpg")
+
 # time module
 clock = pygame.time.Clock()
 clock.tick(100)
 
 # For the caption
 pygame.display.set_caption("Racing")
+
+# Function for adding the background image
+def background():
+    screen.blit(grass, (0,0))
+    screen.blit(grass, (740,0))
+    for i in range(7):
+        screen.blit(yellow_strip,(400,100*i))
+    screen.blit(strip, (120,0))
+    screen.blit(strip, (680,0))
+
 
 # image appearing
 def car(x,y):
@@ -48,10 +66,14 @@ def game_loop():
         x += x_change
 
         # For the background color
+
         screen.fill((119,119,119))
+        background()
 
         # calling the function
         car(x,y)
+        if x > 680 - car_width or x < 110:
+            bumped = True
 
         #updating the game
         pygame.display.update()
