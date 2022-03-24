74d20d2a191177ba04e4d4355e731c69e1df5bfd
lucas-maia-morais <lucasmaiamorais@gmail.com>
2022-03-24 07:37:58 -0300

    Enemy and Game Over

--- car_game.py
+++ car_game.py
@@ -1,6 +1,11 @@
-from turtle import back, width
 import pygame
 pygame.init()
+
+# importing time module
+import time
+
+# importing random module
+import random
 
 # the screen
 width = 800
@@ -18,6 +23,11 @@ grass = pygame.image.load("grass.jpg")
 yellow_strip = pygame.image.load("yellow_strip.jpg")
 strip = pygame.image.load("strip.jpg")
 
+# crashed message
+myfont = pygame.font.SysFont("None", 100)
+render_text = myfont.render("CAR CRASHED", 1, (0,0,0))
+
+
 # time module
 clock = pygame.time.Clock()
 clock.tick(100)
@@ -25,10 +35,29 @@ clock.tick(100)
 # For the caption
 pygame.display.set_caption("Racing")
 
+# Function for the obstacle
+def obstacle(obs_x, obs_y, obs):
+    if obs == 0:
+        obs_pic = pygame.image.load("car2.jpg")
+    elif obs == 1:
+        obs_pic = pygame.image.load("car3.jpg")
+    elif obs == 2:
+        obs_pic = pygame.image.load("car4.jpg")
+    elif obs == 3:
+        obs_pic = pygame.image.load("car5.jpg")
+    elif obs == 4:
+        obs_pic = pygame.image.load("car6.jpg")
+    elif obs == 5:
+        obs_pic = pygame.image.load("car7.jpg")
+
+    # screen.blit(obs_pic, (obs_x,obs_y))
+    screen.blit(obs_pic, (obs_x,obs_y))
+
+
 # Function for adding the background image
 def background():
     screen.blit(grass, (0,0))
-    screen.blit(grass, (740,0))
+    screen.blit(grass, (700,0))
     for i in range(7):
         screen.blit(yellow_strip,(400,100*i))
     screen.blit(strip, (120,0))
@@ -45,11 +74,20 @@ def game_loop():
     x_change = 0
     x = 400
     y = 470
+    obstacle_speed = 10
+    obs = 0
+    y_change = 0
+    obs_x = random.randrange(200, 650)
+    obs_y = -750
+    enemy_width = 56
+    enemy_height = 125
+
 
     #close button
     while not bumped:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                print("Clicked")
                 bumped = True
 
         # moving in x-y coordinates
@@ -65,15 +103,21 @@ def game_loop():
         
         x += x_change
 
-        # For the background color
+        # background color
 
         screen.fill((119,119,119))
         background()
+        # obs_y -= (obstacle_speed/4)
+        obstacle(obs_x,obs_y,obs)
+        obs_y += obstacle_speed
 
         # calling the function
         car(x,y)
         if x > 680 - car_width or x < 110:
-            bumped = True
+            screen.blit(render_text, (100,200))
+            pygame.display.update()
+            time.sleep(5)
+            game_loop()
 
         #updating the game
         pygame.display.update()
